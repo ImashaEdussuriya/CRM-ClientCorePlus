3f0c914d5aa0e97c2631782bf53da1757ffcae76
ImashaEdussuriya <shalindie2003@gmail.com>
2026-04-30 22:08:30 +0530

    Export application models

---
+++ backend/models/index.js
@@ -0,0 +1,11 @@
+const User = require('./User');
+const Customer = require('./Customer');
+const Deal = require('./Deal');
+const Task = require('./Task');
+
+module.exports = {
+  User,
+  Customer,
+  Deal,
+  Task
+};
